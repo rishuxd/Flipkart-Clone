afe031b5ea3022d88f95c89dc95020a18ffe5f19
Jyotiswaroop Srivastav <srivastavjyotiswaroop@gmail.com>
2023-12-21 11:47:04 +0530

    ProductDetails.jsx deploy err

--- client/src/components/details/ProductDetail.jsx
+++ client/src/components/details/ProductDetail.jsx
@@ -24,13 +24,8 @@ const ProductDetail = () => {
     fetchProducts();
   }, [id]);
 
-<<<<<<< HEAD
   if (loading) {
     return <Container1></Container1>;
-=======
-   if (loading) {
-    return <Container1></Container1>
->>>>>>> 459a8a76882c73e389c2bafdcf07a09ae18d3b74
   }
 
   return (
